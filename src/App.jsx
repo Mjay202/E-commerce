df8b092fe70abb6daf87fdb243d88b17536f0040
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-01-10 15:38:59 +0100

    created SingleProduct page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Home from "./pages/Home"
-import ProductList from "./pages/ProductList";
+
+import SingleProduct from "./pages/SingleProduct";
+
+
 
 
 const App = () => {
-  return <ProductList/>;
+  return <SingleProduct/>;
 };
 
 export default App;
---
+++ src/pages/SingleProduct.jsx
@@ -0,0 +1,20 @@
+import Advert from "../components/Advert"
+import Footer from "../components/Footer"
+import NavBar from "../components/NavBar"
+import Newsletter from "../components/Newsletter"
+
+
+
+const SingleProduct = () => {
+  return (
+    <>
+        <NavBar/>
+        <Advert/>
+        <Newsletter/>
+        <Footer/>
+    
+    </>
+  )
+}
+
+export default SingleProduct
