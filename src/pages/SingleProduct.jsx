b27aebcbc24cf8a60a16b4dbbc3e6d64f06850de
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-01-10 17:58:03 +0100

    added Single Product Page

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,8 +1,122 @@
+import styled from "styled-components"
 import Advert from "../components/Advert"
 import Footer from "../components/Footer"
 import NavBar from "../components/NavBar"
 import Newsletter from "../components/Newsletter"
+import { Add, Remove } from "@material-ui/icons"
 
+const Wrapper = styled.div`
+  display: flex;
+  padding: 50px;
+
+`
+const ImgContainer = styled.div`
+  flex: 1;
+  height: 80vh ;
+  width: 100%;
+  margin-right: 20px;
+  
+`
+const Image = styled.img`
+  height: 100%;
+  width: 80%;
+  background-color: #d68080;
+  transform: all 0.5s ease;
+  
+  &:hover {
+    transform: scale(1.1, 1.1)
+  }
+`
+const InfoContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  /* align-items: center;
+  justify-content: flex-start; */
+  padding: 0px 50px;
+`
+const Title = styled.h1`
+  margin-bottom: 30px;
+  font-weight: 300;
+`
+const Desc = styled.p`
+  margin-bottom: 30px;
+`
+const Price = styled.p`
+  font-size: 40px;
+  font-weight: 200;
+`
+const FilterContainer = styled.div`
+  display: flex;
+  width: 50%;
+  justify-content: space-between;
+  margin-top: 20px;
+`
+const FilterColor = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const ColorTitle = styled.h4`
+  font-weight: 300;
+  font-size: 20px;
+  margin-right: 10px;
+
+`
+const ColorOption = styled.span`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  margin-right: 5px;
+  cursor: pointer;
+  background-color: ${(props) => props.color};
+`
+const FilterSize = styled.div`
+  display: flex;
+`
+const SizeTitle = styled.h4`
+  font-weight: 300;
+  font-size: 20px;
+  margin-right: 10px;
+`;
+const SizeOptions = styled.select`
+  padding: 5px;
+`
+const SizeOption = styled.option``
+
+const AddContainer = styled.span`
+  margin-top: 30px;
+  display: flex;
+  justify-content: space-between;
+  width: 50%;
+`
+const AmountContainer = styled.span`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  padding: 0px 5px;
+  font-size: 25px;
+  display: flex;
+  justify-content: center;
+  margin: 0px 10px;
+  border-radius: 20%;
+  background-color: gray;
+`
+const Button = styled.button`
+  padding: 10px;
+  border: 1px solid teal;
+  transition: all 0.5 ease;
+
+  &:hover {
+    background-color: teal;
+    color: white;
+    transform: scale(1.1, 1.1);
+  }
+`
 
 
 const SingleProduct = () => {
@@ -10,6 +124,42 @@ const SingleProduct = () => {
     <>
         <NavBar/>
         <Advert/>
+        <Wrapper>
+          <ImgContainer>
+            <Image/>
+          </ImgContainer>
+          <InfoContainer>
+            <Title>Denim Jumpsuit</Title>
+            <Desc>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt ad debitis, inventore magnam similique in consequatur officia ducimus consectetur quia amet aliquid a excepturi repellat. Repellat quisquam incidunt doloremque provident.</Desc>
+            <Price>$150</Price>
+            <FilterContainer>
+              <FilterColor>
+                <ColorTitle>Color:</ColorTitle>
+                <ColorOption color='black'/>
+                <ColorOption color='darkgray'/>
+                <ColorOption color='gray'/>
+              </FilterColor>
+              <FilterSize>
+                <SizeTitle>Size:</SizeTitle>
+                <SizeOptions>
+                  <SizeOption>XL</SizeOption>
+                  <SizeOption>L</SizeOption>
+                  <SizeOption>M</SizeOption>
+                  <SizeOption>S</SizeOption>
+                  <SizeOption>XS</SizeOption>
+                </SizeOptions>
+              </FilterSize>
+            </FilterContainer>
+            <AddContainer>
+              <AmountContainer>
+                <Remove style={{cursor:'pointer'}}/>
+                <Amount>1</Amount>
+                <Add style={{cursor:'pointer'}}/>
+              </AmountContainer>
+              <Button>Add to Cart</Button>
+            </AddContainer>
+          </InfoContainer>
+        </Wrapper>
         <Newsletter/>
         <Footer/>
     
