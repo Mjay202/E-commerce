2a9f510e725a2427648d6cb809d1479971589856
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-02-04 00:22:09 +0100

    counter fixed

--- client/src/pages/SingleProduct.jsx
+++ client/src/pages/SingleProduct.jsx
@@ -1,36 +1,34 @@
-import styled from "styled-components"
-import Advert from "../components/Advert"
-import Footer from "../components/Footer"
-import NavBar from "../components/NavBar"
-import Newsletter from "../components/Newsletter"
-import { Add, Remove } from "@material-ui/icons"
-import { useEffect, useState } from "react"
-import axios from "axios"
-import { useLocation } from "react-router-dom"
+import styled from "styled-components";
+import Advert from "../components/Advert";
+import Footer from "../components/Footer";
+import NavBar from "../components/NavBar";
+import Newsletter from "../components/Newsletter";
+import { Add, Remove } from "@material-ui/icons";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
 
 const Wrapper = styled.div`
   display: flex;
   padding: 50px;
-
-`
+`;
 const ImgContainer = styled.div`
   flex: 1;
-  height: 80vh ;
+  height: 80vh;
   width: 100%;
   margin-right: 20px;
-  
-`
+`;
 const Image = styled.img`
   height: 100%;
   width: 80%;
   object-fit: cover;
   background-color: #d68080;
   transform: all 0.5s ease;
-  
+
   &:hover {
-    transform: scale(1.1, 1.1)
+    transform: scale(1.1, 1.1);
   }
-`
+`;
 const InfoContainer = styled.div`
   flex: 1;
   display: flex;
@@ -38,35 +36,34 @@ const InfoContainer = styled.div`
   /* align-items: center;
   justify-content: flex-start; */
   padding: 0px 50px;
-`
+`;
 const Title = styled.h1`
   margin-bottom: 30px;
   font-weight: 300;
-`
+`;
 const Desc = styled.p`
   margin-bottom: 30px;
-`
+`;
 const Price = styled.p`
   font-size: 40px;
   font-weight: 400;
-`
+`;
 const FilterContainer = styled.div`
   display: flex;
   width: 50%;
   justify-content: space-between;
   margin-top: 20px;
-`
+`;
 const FilterColor = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 const ColorTitle = styled.h4`
   font-weight: 300;
   font-size: 20px;
   margin-right: 10px;
-
-`
+`;
 const ColorOption = styled.span`
   width: 20px;
   height: 20px;
@@ -74,10 +71,10 @@ const ColorOption = styled.span`
   margin-right: 5px;
   cursor: pointer;
   background-color: ${(props) => props.color};
-`
+`;
 const FilterSize = styled.div`
   display: flex;
-`
+`;
 const SizeTitle = styled.h4`
   font-weight: 300;
   font-size: 20px;
@@ -85,20 +82,20 @@ const SizeTitle = styled.h4`
 `;
 const SizeOptions = styled.select`
   padding: 5px;
-`
-const SizeOption = styled.option``
+`;
+const SizeOption = styled.option``;
 
 const AddContainer = styled.span`
   margin-top: 30px;
   display: flex;
   justify-content: space-between;
   width: 50%;
-`
+`;
 const AmountContainer = styled.span`
   display: flex;
   justify-content: space-between;
   align-items: center;
-`
+`;
 const Amount = styled.span`
   width: 30px;
   height: 30px;
@@ -109,7 +106,7 @@ const Amount = styled.span`
   margin: 0px 10px;
   border-radius: 20%;
   border: 1px solid teal;
-`
+`;
 const Button = styled.button`
   padding: 10px;
   border-radius: 2px;
@@ -121,49 +118,51 @@ const Button = styled.button`
     color: white;
     transform: scale(1.1, 1.1);
   }
-`
-
+`;
 
 const SingleProduct = () => {
   const [product, setproduct] = useState([]);
+  const [quantity, setQuantity] = useState(1);
+  const decreaseQuantity = () => {
+    setQuantity(quantity > 1 ? quantity - 1 : 1);
+  };
+  const increaseQuantity = () => {
+    setQuantity(quantity + 1);
+  };
   const location = useLocation();
   const prodId = location.pathname.split("/")[2];
-  console.log(prodId);
 
-  // useEffect(() => {
-  //   const getProduct = async () => {
-  //     try {
-  //       const res = await axios.get("http://localhost:5200/api/products/");
-  //       console.log(res.data);
-  //       setproduct(res.data);
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   };
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:5200/api/products/${prodId}`
+        );
+        console.log(res.data);
+        setproduct(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-  //   getProduct();
-  // }, []);
+    getProduct();
+  }, []);
   return (
     <>
       <NavBar />
       <Advert />
       <Wrapper>
         <ImgContainer>
-          <Image src="https://img.freepik.com/free-photo/woman-model-demonstrating-winter-cloths_1303-16949.jpg?w=360&t=st=1706837059~exp=1706837659~hmac=c6d300421eec096293b478906008eae13a9aec94a720cfd56828895a7ae3a4d2" />
+          <Image src={product.img} />
         </ImgContainer>
         <InfoContainer>
-          <Title>Denim Jumpsuit</Title>
-          <Desc>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt ad
-            debitis, inventore magnam similique in consequatur officia ducimus
-            consectetur quia amet aliquid a excepturi repellat. Repellat
-            quisquam incidunt doloremque provident.
-          </Desc>
-          <Price>$150</Price>
+          <Title>{product.title}</Title>
+          <Desc>{product.desc}</Desc>
+          <Price>{product.price}</Price>
           <FilterContainer>
             <FilterColor>
               <ColorTitle>Color:</ColorTitle>
-              <ColorOption color="black" />
+              <ColorOption color={product.color} />
               <ColorOption color="darkgray" />
               <ColorOption color="gray" />
             </FilterColor>
@@ -180,9 +179,13 @@ const SingleProduct = () => {
           </FilterContainer>
           <AddContainer>
             <AmountContainer>
-              <Remove style={{ cursor: "pointer" }} />
-              <Amount>1</Amount>
-              <Add style={{ cursor: "pointer" }} />
+              <Remove
+                onClick={decreaseQuantity}
+                style={{ cursor: "pointer" }}
+              />
+
+              <Amount>{quantity}</Amount>
+              <Add onClick={increaseQuantity} style={{ cursor: "pointer" }} />
             </AmountContainer>
             <Button>Add to Cart</Button>
           </AddContainer>
@@ -192,6 +195,6 @@ const SingleProduct = () => {
       <Footer />
     </>
   );
-}
+};
 
-export default SingleProduct
+export default SingleProduct;
