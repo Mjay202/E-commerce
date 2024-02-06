91cbd14e30e6f546ccd15bee0939de7bb7baf6a1
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-02-07 00:50:31 +0100

    cart post

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -1,19 +1,28 @@
 import express from "express";
-import { verifyTokenAndAuthorization } from "./verifyToken";
+import { verifyTokenAndAuthorization } from "./verifyToken.js";
 const router = express.Router();
-
+import Cart from "../models/Cart.js";
 
 // CREATE CART
 
-router.get("/:id", verifyTokenAndAuthorization, (req, res) => {
-    
-})
+router.post("/:id", async (req, res) => {
+  const newCart = new Cart(req.body);
+
+  try {
+    const savedCart = await newCart.save();
+    res.status(200).json(savedCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // UPDATE CART
 
 //DELETE CART
 
 // GET CART (takes user_id)
+
+
 
 // GET ALL CART (for Admin)
 
