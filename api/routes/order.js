4887fce26cdbe3c123c14af3ffc941868bb79975
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-02-08 17:08:25 +0100

    order complete

--- api/routes/order.js
+++ api/routes/order.js
@@ -14,36 +14,58 @@ router.post("/:id", async (req, res) => {
 });
 
 // update (only admin)
-router.put("/:id", async(req, res) => {
+router.put("/:id", async (req, res) => {
     const orderId = req.params.id
 
     try {
         const updatedOrder = await Order.findByIdAndUpdate(
             orderId,
             { $set: req.body },
-            { new: true}
+            { new: true }
         )
         res.status(200).json(updatedOrder);
     } catch (err) {
         res.status(500).json(err)
     }
-})
+});
+
 // delete (only admin)
 router.delete("/:id", async (req, res) => {
-     const orderId = req.params.id;
+    const orderId = req.params.id;
     try {
         await Order.findByIdAndDelete(orderId)
         res.status(200).json("Deleted successfully")
     } catch (err) {
         res.status(500).json(err)
     }
-})
+});
 
 // get orders
 router.get("/:id", async (req, res) => {
-    
-})
+    try {
+        const orders = await Order.find({ userId: req.params.id })
+        res.status(200).json(orders)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
 // get all orders (only admin)
+router.get("/", async (req, res) => {
+    try {
+        const allOrders = await Order.find()
+        res.status(200).json(allOrders)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 // aggregate sales by last two months
+router.get("/", async (req, res) => {
+    try {
+        const sales = await Order.find().sort()
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 export default router;
